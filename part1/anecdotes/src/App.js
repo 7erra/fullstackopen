8e60ab72299f6e50beba3e00cc4440620e2c61c8
Terra <ichbinterra@gmail.com>
2023-03-11 00:31:36 +0100

    Exercise 1.14

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const Anecdote = ({ content, votes }) => {
+  return (
+    <div>
+      <p>{content}</p>
+      <p>has {votes} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(anecdotes.map((_) => 0))
+  const [mostVoted, setMostVoted] = useState(0)
+
+  function selectRandomAnecdote() {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  function castVote() {
+    const copy = [...votes]
+    let newVote = copy[selected] + 1
+    copy[selected] = newVote
+    setVotes(copy)
+    if (newVote > copy[mostVoted]) {
+      setMostVoted(selected)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote content={anecdotes[selected]} votes={votes[selected]} />
+      <button onClick={castVote}>vote</button>
+      <button onClick={selectRandomAnecdote}>next anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      <Anecdote content={anecdotes[mostVoted]} votes={votes[mostVoted]} />
+    </div>
+  )
+}
+
+export default App
