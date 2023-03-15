5dd708bd570f18ae6a921342fd9fd1275de3b2f9
Terra <ichbinterra@gmail.com>
2023-03-15 14:26:49 +0100

    Exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,12 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 app.use(express.json())
+morgan.token("data", (request) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(":method :url :status - :total-time ms :data"))
 
 let persons = [
   {
