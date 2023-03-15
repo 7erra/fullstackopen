4d7cc6cc9ea7787d449ca9036295ea9e399010a4
Terra <ichbinterra@gmail.com>
2023-03-15 20:32:08 +0100

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 morgan.token("data", (request) => {
   return JSON.stringify(request.body)
 })
 
 app.use(morgan(":method :url :status - :total-time ms :data"))
+app.use(express.static("build"))
 
 let persons = [
   {
@@ -80,7 +84,7 @@ app.post("/api/persons", (request, response) => {
   response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
