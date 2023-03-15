668736178ab8cf8040d0e187cf3de990d9cb1862
Terra <ichbinterra@gmail.com>
2023-03-15 01:14:22 +0100

    Exercise 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,8 @@
-import express from "express"
+const express = require("express")
 const app = express()
 app.use(express.json())
 
-const persons = [
+let persons = [
   {
     "id": 1,
     "name": "Arto Hellas",
@@ -25,8 +25,53 @@ const persons = [
   }
 ]
 
-app.get("/api/persons", (request, response) => {
-  response.send(persons)
+app.get("/api/persons", (_, response) => {
+  response.json(persons)
+})
+
+app.get("/info", (_, response) => {
+  response.send(`Phonebook has info for ${persons.length} people.<br/>${new Date()}`)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+  response.send()
+})
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+
+  let message;
+  if (!body.name) {
+    message = "Name is missing"
+  } else if (!body.number) {
+    message = "Number is missing"
+  } else if (persons.find(p => p.name === body.name)) {
+    message = "Name must be unique"
+  }
+
+  if (message) {
+    return response.status(400).json({ error: message })
+  }
+
+  const newPerson = {
+    id: Math.round(Math.random() * Number.MAX_SAFE_INTEGER),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
 })
 
 const PORT = 3001
