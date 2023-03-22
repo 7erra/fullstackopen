3e2efd7a2e184e854c7c82884d6d519dfa1b6a1e
Terra <ichbinterra@gmail.com>
2023-03-22 21:41:54 +0100

    Exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
 const config = require("./utils/config")
 const logger = require("./utils/logger")
 const middleware = require("./utils/middleware")
@@ -18,6 +19,7 @@ app.use(
 )
 app.use("/api/blogs/", blogsRouter)
 app.use("/api/users/", usersRouter)
+app.use("/api/login/", loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
