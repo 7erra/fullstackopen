4dfe2e940aa4da75d29d8f5a0b54c74d8e26aa01
Terra <ichbinterra@gmail.com>
2023-03-27 19:21:24 +0200

    Exercise 5.7

--- part5/frontend/src/components/Blog.js
+++ part5/frontend/src/components/Blog.js
@@ -1,7 +1,26 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={() => setVisible(!visible)}>{visible ? "Hide" : "View"}</button>
+      <div style={{ display: visible ? "" : "none" }}>
+        {blog.author}<br />
+        {blog.url}<br />
+        {blog.likes} <button>Like</button><br />
+        {blog.author}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
